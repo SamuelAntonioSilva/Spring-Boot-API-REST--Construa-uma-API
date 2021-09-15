739365ea68168c10be9d8c8bea0a718830617080
SamuelAntonioSilva <samuel.antonio.silva@outlook.com>
2021-09-14 23:07:28 -0300

    Projeto inicial Alura
    Primeira versão disponibilizada no curso.

---
+++ forum/src/test/java/br/com/akura/forum/ForumApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.akura.forum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
